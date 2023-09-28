4dbd49b46406a58d3cdd01154f23a5671c69f16b
alcatrazzPlayfair <jcjohnny5@gmail.com>
2023-09-28 11:39:06 -0700

    updated success and failure page links again and added attachment container back to template

--- attachmentHandler.js
+++ attachmentHandler.js
@@ -7,8 +7,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
         let formStart = '<form action="' + (window.isMailboxForm ? "/app/secureForms/submitFormAsMessage" : "/app/secureForms/submit") + 
         '" method="POST" enctype="multipart/form-data" name="form_name_goes_here">' +
         '<input type="hidden" value="form_name_goes_here" name="sf:form_name" />' +
-        '<input type="hidden" name="sf:success_page" value="/success_page_link_goes_here" />' +
-        '<input type="hidden" name="sf:failure_page" value="/failure_page_link_goes_here" />';
+        '<input type="hidden" name="sf:success_page" value="/p_page.cfm?pptitle=success%5Fsponsor%5Fpage" />' +
+        '<input type="hidden" name="sf:failure_page" value="/p_page.cfm?pptitle=failure%5Fsponsor%5Fpage" />';
 
         if (window.isMailboxForm) {
             formStart += `<input type="hidden" name="sf:recipient_mailbox" value="${window.mailboxID || 'mailbox_id_number_goes_here'}">`;
@@ -16,7 +16,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
         let attachmentSection = '';
         if (hasAttachment) {
-            attachmentSection = '<script src="/lib/secureForms/attachments.js"></script>' +
+            attachmentSection = '<div class="attachments-titleContainer"><div class="subtitle attachments-title">Secure Attachments</div><span class="attachments-title-hint">Max 20 files, 126MB file size</span></div>' +
+            '<script src="/lib/secureForms/attachments.js"></script>' +
             '<div id="sf:attachmentsContainer"></div>' +
             '<button id="sf:addAttachment" class="btn">Add Attachment</button>';
         }
