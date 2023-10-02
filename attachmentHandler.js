b1d2ab966ae2b098fb3ca617f8dba19ac57ed391
alcatrazzPlayfair <jcjohnny5@gmail.com>
2023-10-02 09:17:21 -0700

    removed async and defer from attachmentHandler script tag in index and attached the main function that hanldes that external script to the window to ensure it is available so that the error will be fixed where it can't find it.

--- attachmentHandler.js
+++ attachmentHandler.js
@@ -1,35 +1,33 @@
 // Declare a global variable to check if the form has an attachment or not
 var hasAttachment = false;
 
-document.addEventListener("DOMContentLoaded", function(event) { 
+// The function is now defined in the global scope, outside of the event listener
+window.addAttachmentToForm = function(content) {
+    let formStart = '<form action="' + (window.isMailboxForm ? "/app/secureForms/submitFormAsMessage" : "/app/secureForms/submit") + 
+    '" method="POST" enctype="multipart/form-data" name="form_name_goes_here">' +
+    '<input type="hidden" value="form_name_goes_here" name="sf:form_name" />' +
+    '<input type="hidden" name="sf:success_page" value="/success_page_link_goes_here" />' +
+    '<input type="hidden" name="sf:failure_page" value="/failure_page_link_goes_here" />';
 
-    window.addAttachmentToForm = function(content) {
-        let formStart = '<form action="' + (window.isMailboxForm ? "/app/secureForms/submitFormAsMessage" : "/app/secureForms/submit") + 
-        '" method="POST" enctype="multipart/form-data" name="form_name_goes_here">' +
-        '<input type="hidden" value="form_name_goes_here" name="sf:form_name" />' +
-        '<input type="hidden" name="sf:success_page" value="/success_page_link_goes_here" />' +
-        '<input type="hidden" name="sf:failure_page" value="/failure_page_link_goes_here" />';
-
-        if (window.isMailboxForm) {
-            formStart += '<input type="hidden" name="sf:recipient_mailbox" value="mailbox_id_number_goes_here">';
-        }
-
-        let attachmentSection = '';
-        if (hasAttachment) {
-            attachmentSection = '<script src="/lib/secureForms/attachments.js"></script>' +
-            '<div id="sf:attachmentsContainer"></div>' +
-            '<button id="sf:addAttachment" class="btn">Add Attachment</button>';
-        }
-
-        const formContent = content + attachmentSection;
-        
-        const formEnd = '<div class="submit_btn">' +
-            '<input type="submit" value="Submit">' +
-            '</div></form>';
-
-        return formStart + formContent + formEnd;
+    if (window.isMailboxForm) {
+        formStart += '<input type="hidden" name="sf:recipient_mailbox" value="mailbox_id_number_goes_here">';
     }
-});
+
+    let attachmentSection = '';
+    if (hasAttachment) {
+        attachmentSection = '<script src="/lib/secureForms/attachments.js"></script>' +
+        '<div id="sf:attachmentsContainer"></div>' +
+        '<button id="sf:addAttachment" class="btn">Add Attachment</button>';
+    }
+
+    const formContent = content + attachmentSection;
+    
+    const formEnd = '<div class="submit_btn">' +
+        '<input type="submit" value="Submit">' +
+        '</div></form>';
+
+    return formStart + formContent + formEnd;
+};
 
 function toggleAttachmentStatus(buttonElement) {
     console.log("toggleAttachmentStatus called. Previous state:", hasAttachment);
@@ -37,3 +35,10 @@ function toggleAttachmentStatus(buttonElement) {
     console.log("New state:", hasAttachment);
     buttonElement.innerText = hasAttachment ? "Remove Attachment from Form" : "Add Attachment to Form";
 }
+
+window.toggleAttachmentStatus = toggleAttachmentStatus; // Set it in global scope
+
+// If you have other functionalities that rely on DOMContentLoaded, keep them separate.
+document.addEventListener("DOMContentLoaded", function(event) {
+    // other code that relies on DOM being ready
+});
